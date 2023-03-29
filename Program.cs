ccd39ee52fa457128f20659c28d9794a12a5f3c0
Sided911 <dima.puluchu.05@mail.ru>
2023-03-30 00:28:00 +0300

    zadanie 1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,49 @@ namespace ConsoleApp15
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введите количество элементов в массиве: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(1, 81);
+            }
+            Console.WriteLine("Исходный массив: ");
+            PrintArray(arr);
+            int minIndex = 0;
+            int maxIndex = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] < arr[minIndex])
+                {
+                    minIndex = i;
+                }
 
+                if (arr[i] > arr[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            Console.WriteLine("Минимальный элемент массива: " + arr[minIndex]);
+            Console.WriteLine("Максимальный элемент массива: " + arr[maxIndex]);
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[maxIndex];
+            arr[maxIndex] = temp;
+            Console.WriteLine("Массив после обмена: ");
+            PrintArray(arr);
+
+            Console.ReadLine();
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
